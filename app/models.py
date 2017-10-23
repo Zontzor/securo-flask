2055ebe0aadc97fc525349a59b2d54ceb727f343
Zontzor <zontzor@gmail.com>
2017-10-23 01:06:22 +0100

    Remove user model

--- app/models.py
+++ app/models.py
@@ -1,44 +1,4 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
 from app import db
-
-class User(db.Model):
-    """
-    Create an user table
-    """
-
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(60), index=True, unique=True)
-    username = db.Column(db.String(60), index=True, unique=True)
-    first_name = db.Column(db.String(60), index=True)
-    last_name = db.Column(db.String(60), index=True)
-    password_hash = db.Column(db.String(128))
-    is_admin = db.Column(db.Boolean, default=False)
-
-    @property
-    def password(self):
-        """
-        Prevent password from being accessed
-        """
-        raise AttributeError('password is not a readable attribute.')
-
-    @password.setter
-    def password(self, password):
-        """
-        Set password to a hashed password
-        """
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        """
-        Check if hashed password matches actual password
-        """
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User: {}>'.format(self.username)
 
 class Photo(db.Model):
     """
